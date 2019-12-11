7c2ac405734577424945a493af6c0fd45f3fcf39
Lioscro <kmin@caltech.edu>
2019-12-10 17:49:36 -0800

    1.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = read('README.md')
 
 setup(
     name='bebi103_9_2',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/Lioscro/bebi103-9-2',
     description='Pip-installable package with functions used in hw9.2',
     long_description=long_description,
